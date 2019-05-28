25faccb027a72b8dc746df50250513b9953dc61a
Long <fchbyoung1010@gmail.com>
2019-05-28 21:44:06 +0700

    Long nộp bài tập diện tích HCN

---
+++ dientich_hcn.js
@@ -0,0 +1,7 @@
+let a = prompt("Nhập chiêu rộng: ");
+let b = prompt("Nhập chiều cao: ");
+let width = parseInt(a);
+let height = parseInt(b);
+let area = width * height;
+
+document.write("area = " + area);
